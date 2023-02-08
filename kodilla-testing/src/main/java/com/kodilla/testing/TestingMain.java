105d7a73d5d8ff219316ef5fd1b0d859d4fb3017
PrzemekT <przemyslaw.trebicki@gmail.com>
2023-02-08 22:16:31 +0100

    6.2 Create Calculator class and test it.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+import java.util.logging.SimpleFormatter;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK!");
+        } else {
+            System.out.println("Error!\nUser name do not match.");
+        }
+        System.out.println("----- ----- -----");
+        Calculator calculator = new Calculator(3, 2);
+        int adding = calculator.add();
+        if (adding == 5) {
+            System.out.println("Funkcja dodawania jest OK!");
+        } else {
+            System.out.println("Funkcja dodawania nie dziala!");
+        }
+        //System.out.println("Wynik dodawania, metoda 1: "+calculator.add());
+        //System.out.println("Wynik dodawania metoda 2: "+adding);
+        if (calculator.subtract() == 1) {
+            System.out.println("Funkcja odejmowania jest OK!");
+        } else {
+            System.out.println("Funkcja odejmowania nie dziala!");
+        }
+
+        //System.out.println("Wynik odejmowania: "+calculator.subtract());
     }
 }
